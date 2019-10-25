0c157c6b8ba7085413fd220b26c5562018ca9589
ayushjainrksh <ayushjainrksh@gmail.com>
2019-10-26 03:55:23 +0530

    Added threads for parallel script execution

--- client.py
+++ client.py
@@ -9,25 +9,53 @@ class Client:
         # self.listen_socket.listen(1)
 
         # print(f'Serving HTTP on port {PORT} ...')
-        
+        i_thread = threading.Thread(target=self.send_message)
+        i_thread.daemon = True
+        i_thread.start()
+
         while True:
+
+            r_thread = threading.Thread(target=self.receive_message)
+            r_thread.start()
+            r_thread.join()
+
             data = self.receive_message()
 
             if not data:
                 print("Server failed")
                 break
 
-            elif data[0:1]   == b'\x11':
+            elif data[0:1]  == b'\x11':
                 print("Got peers")
                 self.update_peers(data[1:])
                 print(data)
 
+    def send_message(self):
+        try:
+            self.listen_socket.send("req".encode('utf-8'))
+
+        except KeyboardInterrupt:
+            self.send_disconnect_signal()
+            return
+
+
     def receive_message(self):
-        print("Receiving...")
-        data = self.listen_socket.recv(1024)
-        print(data.decode('utf-8'))
-        print("Received message is : ")
-        return data
+        try:
+            print("Receiving...")
+            data = self.listen_socket.recv(1024)
+            print("Received message is : ")
+            print(data.decode('utf-8'))
+            return data
+        except KeyboardInterrupt:
+            self.send_disconnect_signal()
 
     def update_peers(self, peers):
+        print("--------  ", peers)
+        print("YO : ", p2p.peers)
         p2p.peers = str(peers, "utf-8").split(',')[:-1]
+
+
+    def send_disconnect_signal(self):
+        print("Disconnected from server")
+        self.listen_socket.send("q".encode('utf-8'))
+        sys.exit()
--- server.py
+++ server.py
@@ -3,30 +3,69 @@ from libs import *
 class Server:
 
     def __init__(self,msg):
-        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            self.msg = msg
+            self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        self.connections = []
-        self.peers = []
-        
-        self.listen_socket.bind((HOST, PORT))
-        self.listen_socket.listen(1)
+            self.connections = []
+            self.peers = []
+            
+            self.listen_socket.bind((HOST, PORT))
+            self.listen_socket.listen(3)
 
-        print(f'Serving HTTP on port {PORT} ...')
-        
-        self.run()
+            print(f'Serving HTTP on port {PORT} ...')
+            
+            self.run()
+
+        except KeyboardInterrupt:
+            print("Disconnected")
+            sys.exit()
+
+    def handler(self, client_connection, client_address):
+        try: 
+            while True:
+
+                data = client_connection.recv(1024)
+                print(data.decode('utf-8'))
+                for connection in self.connections:
+                    # print(connection)
+                    if data and data.decode('utf-8')[0].lower == 'q':
+                        self.disconnect(connection, client_address)
+                        return
+                    
+                    elif data and data.decode('utf-8') == "req":
+                        print("Uploading")
+                        connection.send(self.msg.encode('utf-8'))
+                        return
+                        
+        except KeyboardInterrupt:
+            sys.exit()
+
+    def disconnect(self, client_connection, client_address):
+        self.connections.remove(client_connection)
+        self.peers.remove(client_address)
+        client_connection.close()
+        self.send_peers()
+        print("{}, disconnected".format(client_address))
+
 
     
     def run(self):
+
         while True:
             client_connection, client_address = self.listen_socket.accept()
             
             self.peers.append(client_address)
-            print(self.peers)
+            print("Peers : ", self.peers)
             self.send_peers()
+            
+            c_thread = threading.Thread(target=self.handler, args=(client_connection, client_address))
+            c_thread.daemon = True
+            c_thread.start()
 
             self.connections.append(client_connection)
-            print(client_connection, " added")
+            print("{}, connected".format(client_address))
 
         #     request_data = client_connection.recv(1024)
         #     print(request_data.decode('utf-8'))
@@ -41,9 +80,13 @@ class Server:
     def send_peers(self):
         listOfPeers = ""
         for peer in self.peers:
-            listOfPeers += str(peer[0])+", "
-        
+            # print("- -- -", peer[0], "- -- -")
+            listOfPeers = listOfPeers + str(peer[0]) + ", "
+        # print("----", listOfPeers, "----")
+
         for connection in self.connections:
-            data  =  '\x11' + bytes(listOfPeers, 'utf-8')
-            connection.send('\x11' + bytes(listOfPeers, 'utf-8'))
+            data  =  b'\x11' + bytes(listOfPeers, 'utf-8')
+            connection.send(b'\x11' + bytes(listOfPeers, 'utf-8'))
+            print(data)
+            print(connection)
 # server = Server("HELLO")
